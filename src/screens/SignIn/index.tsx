35314ba4010b490b209c68e62ab0fcad59715763
renamcomn <acidmud@gmail.com>
2022-04-28 21:24:49 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+    View,
+    Text
+} from 'react-native';
+
+export function SignIn() {
+    return(
+        <View>
+            <Text>
+                Tela de Login
+            </Text>
+        </View>
+    )
+}
