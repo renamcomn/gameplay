11d845fe6ca41e8248f32e992f000f93bda60324
renamcomn <acidmud@gmail.com>
2022-04-28 23:08:28 -0300

    react navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading'
-
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +18,13 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <SignIn />
+    <>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor="transparent"
+      />
+      <Routes />
+    </>
+   
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -11,42 +11,45 @@ import { styles } from './styles';
 
 import backgroundImg from '../../assets/illustration.png';
 import { ButtonIcon } from "../../components/ButtonIcon";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignIn() {
-    const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
 
-     async function handleSignIn() {
+  const [loading, setLoading] = useState(false);
 
-    }
+  async function handleSignIn() {
+    navigation.navigate('Home');
+  }
 
-    return(
-      <View style={styles.container}>     
-        <Image 
-          source={backgroundImg} 
-          style={styles.image} 
-          resizeMode="stretch"
-        />
+  return(
+    <View style={styles.container}>     
+      <Image 
+        source={backgroundImg} 
+        style={styles.image} 
+        resizeMode="stretch"
+      />
 
-        <View style={styles.content}>
-          <Text style={styles.title}>
-            Conecte-se {'\n'}
-            e organize suas {'\n'} 
-            jogatinas
-          </Text>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {'\n'}
+          e organize suas {'\n'} 
+          jogatinas
+        </Text>
 
-          <Text style={styles.subtitle}>
-            Crie grupos para jogar seus games {'\n'} 
-            favoritos com seus amigos
-          </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'} 
+          favoritos com seus amigos
+        </Text>
 
-          {
-            loading ? <ActivityIndicator color={theme.colors.primary} /> :
-            <ButtonIcon 
-                title="Entrar com Discord"
-                onPress={handleSignIn}
-            />  
-          }                             
-        </View>
+        {
+          loading ? <ActivityIndicator color={theme.colors.primary} /> :
+          <ButtonIcon 
+              title="Entrar com Discord"
+              onPress={handleSignIn}
+          />  
+        }                             
       </View>
-    )
+    </View>
+  )
 }
